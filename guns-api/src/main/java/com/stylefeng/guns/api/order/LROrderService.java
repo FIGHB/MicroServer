7d7179e7aa1c679ff163122dca7b846cf00f1376
FIGHB <li839521927@163.com>
2019-10-16 16:11:42 +0800

    author:李锐

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/order/LROrderService.java
@@ -0,0 +1,5 @@
+package com.stylefeng.guns.api.order;
+
+public interface LROrderService {
+
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/gateway/modular/order/LROrderController.java
@@ -0,0 +1,7 @@
+package com.stylefeng.guns.gateway.modular.order;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LROrderController {
+}
