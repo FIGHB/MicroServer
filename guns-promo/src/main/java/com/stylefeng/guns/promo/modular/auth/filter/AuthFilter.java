058fb7c7bb7df0a86d126be9898616a661259689
SteveYangSunshine <steveyangsunshine@gmail.com>
2019-10-18 12:04:31 +0800

    @author 李锐

--- guns-promo/src/main/java/com/stylefeng/guns/promo/modular/auth/filter/AuthFilter.java
+++ guns-promo/src/main/java/com/stylefeng/guns/promo/modular/auth/filter/AuthFilter.java
@@ -1,16 +1,15 @@
-package com.stylefeng.guns.gateway.modular.auth.filter;
+package com.stylefeng.guns.promo.modular.auth.filter;
 
 import com.stylefeng.guns.core.base.tips.ErrorTip;
 import com.stylefeng.guns.core.util.RenderUtil;
-import com.stylefeng.guns.gateway.common.exception.BizExceptionEnum;
-import com.stylefeng.guns.gateway.config.properties.JwtProperties;
-import com.stylefeng.guns.gateway.modular.auth.util.JwtTokenUtil;
+import com.stylefeng.guns.promo.common.exception.BizExceptionEnum;
+import com.stylefeng.guns.promo.modular.auth.util.JwtTokenUtil;
+import com.stylefeng.guns.promo.config.properties.JwtProperties;
 import io.jsonwebtoken.JwtException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
-import redis.clients.jedis.Jedis;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -32,26 +31,14 @@ public class AuthFilter extends OncePerRequestFilter {
     private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
-    private Jedis jedis;
-
-    @Autowired
     private JwtProperties jwtProperties;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        /*if (request.getServletPath().equals("/" + jwtProperties.getAuthPath())) {
+        if (request.getServletPath().equals("/" + jwtProperties.getAuthPath())) {
             chain.doFilter(request, response);
             return;
-        }*/
-        String ignoreUrl = jwtProperties.getIgnoreUrl();
-        String[] split = ignoreUrl.split(",");
-        for (int i = 0; i <split.length; i++) {
-            if (request.getServletPath().contains(split[i])){
-                chain.doFilter(request, response);
-                return;
-            }
         }
-
         final String requestHeader = request.getHeader(jwtProperties.getHeader());
         String authToken = null;
         if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
@@ -59,22 +46,11 @@ public class AuthFilter extends OncePerRequestFilter {
 
             //验证token是否过期,包含了验证jwt是否正确
             try {
-                jwtTokenUtil.parseToken(authToken);
-                String username = jedis.get(authToken);
-                if(username==null){
-                    RenderUtil.renderJson(response, new ErrorTip(BizExceptionEnum.TOKEN_EXPIRED.getCode(), BizExceptionEnum.TOKEN_EXPIRED.getMessage()));
-                    return;
-                }else {
-                    jedis.expire(authToken,604800);
-                }
-
-                request.setAttribute("token",authToken);
-
-               /* boolean flag = jwtTokenUtil.isTokenExpired(authToken);
+                boolean flag = jwtTokenUtil.isTokenExpired(authToken);
                 if (flag) {
                     RenderUtil.renderJson(response, new ErrorTip(BizExceptionEnum.TOKEN_EXPIRED.getCode(), BizExceptionEnum.TOKEN_EXPIRED.getMessage()));
                     return;
-                }*/
+                }
             } catch (JwtException e) {
                 //有异常就是token解析失败
                 RenderUtil.renderJson(response, new ErrorTip(BizExceptionEnum.TOKEN_ERROR.getCode(), BizExceptionEnum.TOKEN_ERROR.getMessage()));
